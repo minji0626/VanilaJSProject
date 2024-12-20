e34fad6bf024c88ffdeaf6439629d9d6a76c9875
minji0626 <jo010163@naver.com>
2024-12-20 11:06:18 +0900

    still writing

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+//const loginButton = document.querySelector("#login-form button");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    console.log(username);
+}
+
+function handleLinkClick(){
+    alert("clicked!!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
